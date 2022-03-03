767c5abea158641c1c531dde3f2b6d4713290ccf
Nerbelle <nelbin_john2005@yahoo.com>
2022-03-03 18:04:32 +0800

    fix: Update for ios15
    Update centerViewController from clear to black

--- MMDrawerController/MMDrawerController.m
+++ MMDrawerController/MMDrawerController.m
@@ -408,7 +408,7 @@ static NSString *MMDrawerOpenSideKey = @"MMDrawerOpenSide";
     if(_centerContainerView == nil){
         _centerContainerView = [[MMDrawerCenterContainerView alloc] initWithFrame:centerFrame];
         [self.centerContainerView setAutoresizingMask:UIViewAutoresizingFlexibleWidth|UIViewAutoresizingFlexibleHeight];
-        [self.centerContainerView setBackgroundColor:[UIColor clearColor]];
+        [self.centerContainerView setBackgroundColor:[UIColor blackColor]];
         [self.centerContainerView setOpenSide:self.openSide];
         [self.centerContainerView setCenterInteractionMode:self.centerHiddenInteractionMode];
         [self.childControllerContainerView addSubview:self.centerContainerView];
